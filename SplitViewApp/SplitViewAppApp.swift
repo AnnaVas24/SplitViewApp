4a015971a4de7c7a81200d6ef29d2a8d7e5f76a9
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2022-11-11 15:02:40 +0300

    Initial Commit

---
+++ SplitViewApp/SplitViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitViewAppApp.swift
+//  SplitViewApp
+//
+//  Created by Vasichko Anna on 11.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SplitViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
